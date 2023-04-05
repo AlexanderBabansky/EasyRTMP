778c5d3ce69861b80bc61b1f722789872b74e6bb
Alexander Babansky <babanscky.sasha@gmail.com>
2023-04-05 16:32:51 +0300

    Clean conan file

--- conanfile.py
+++ conanfile.py
@@ -9,25 +9,20 @@ class EasyRTMP(ConanFile):
     exports_sources = "CMakeLists.txt", "src/*", "include/*", "cmake/*"
 
     def layout(self):
-        print("LAYOUT")
         cmake_layout(self)
 
     def generate(self):
-        print("GENERATE")
         tc = CMakeToolchain(self)
         tc.generate()
 
     def build(self):
-        print("BUILD")
         cmake = CMake(self)
         cmake.configure()
         cmake.build()
 
     def package(self):
-        print("PACKAGE")
         cmake = CMake(self)
         cmake.install()
 
     def package_info(self):
-        print("PACKAGE INFO")
         self.cpp_info.libs = ["easyrtmp"]
