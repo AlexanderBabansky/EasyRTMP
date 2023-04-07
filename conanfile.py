dc4721b2738e78cf3a7acfb6004b274f96bad734
Alexander Babansky <babanscky.sasha@gmail.com>
2023-04-07 10:26:10 +0300

    Copy pdb to package

--- conanfile.py
+++ conanfile.py
@@ -1,5 +1,7 @@
 from conan import ConanFile
 from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout
+from conan.tools.files import copy
+import os
 
 class EasyRTMP(ConanFile):
     name = "easyrtmp"
@@ -23,6 +25,8 @@ class EasyRTMP(ConanFile):
     def package(self):
         cmake = CMake(self)
         cmake.install()
+        copy(self, "*.pdb", src=os.path.join(self.build_folder, str(self.settings.build_type)),
+             dst=os.path.join(self.package_folder, "bin"))
 
     def package_info(self):
         self.cpp_info.libs = ["easyrtmp"]
